52829d75cdc7aeee50713ef04e6ae363588e842a
Szalló László <szallol@gmail.com>
2024-09-15 21:01:19 +0300

    egui mockup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,164 @@
 use anyhow::{Context, Result};
-use obj::{load_obj, Obj};
+use eframe::egui;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{self, BufRead};
+use std::path::Path;
 
 fn main() -> Result<()> {
-    let input = BufReader::new(File::open(
-        "c:/work/_help/terra_obj/BlockBABB/BlockBABB.obj ",
-    )?);
-    let model: Obj = load_obj(input)?;
-    model.vertices.iter().for_each(|v| {
-        println!("v: {:?}", v);
-    });
+    let lines = read_lines("c:/work/_help/_sunix/terra_obj/BlockBABY/BlockBABY.obj")?;
+
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([640.0, 240.0]) // wide enough for the drag-drop overlay text
+            .with_drag_and_drop(true),
+        ..Default::default()
+    };
+    let _ = eframe::run_native(
+        ".obj vertex translator",
+        options,
+        Box::new(|_cc| Ok(Box::<MyApp>::default())),
+    );
+
+    // for line in lines.flatten() {
+    //     match &line[..2] {
+    //         "v " => {
+    //             println!("{}", translate_vertex(&line)?)
+    //         }
+    //         _ => println!("{}", line),
+    //     }
+    // }
 
     Ok(())
+}
+
+fn translate_vertex(vstr: &str) -> Result<String> {
+    let mut cols = vstr
+        .split_ascii_whitespace()
+        .skip(1)
+        .map(|s| s.parse::<f32>().unwrap());
+
+    let x = cols
+        .next()
+        .with_context(|| format!("failed to parse X in line: {}", vstr))?
+        + 498521.12878285768;
+    let y = cols
+        .next()
+        .with_context(|| format!("failed to parse Y in line: {}", vstr))?
+        + 389878.04510264623;
+    let z = cols
+        .next()
+        .with_context(|| format!("failed to parse Z in line: {}", vstr))?
+        + 545.18800000022418;
+    Ok(format!("v {} {} {}", x, y, z))
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Default)]
+struct MyApp {
+    dropped_files: Vec<egui::DroppedFile>,
+    obj_source_path: Option<String>,
+    metadata_path: Option<String>,
+    dest_path: Option<String>,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Drag-and-drop files onto the window!");
+
+            if ui.button("Open file…").clicked() {
+                if let Some(path) = rfd::FileDialog::new().pick_file() {
+                    self.obj_source_path = Some(path.display().to_string());
+                }
+            }
+
+            if let Some(picked_path) = &self.obj_source_path {
+                ui.horizontal(|ui| {
+                    ui.label("Picked file:");
+                    ui.monospace(picked_path);
+                });
+            }
+
+            // Show dropped files (if any):
+            if !self.dropped_files.is_empty() {
+                ui.group(|ui| {
+                    ui.label("Dropped files:");
+
+                    for file in &self.dropped_files {
+                        let mut info = if let Some(path) = &file.path {
+                            path.display().to_string()
+                        } else if !file.name.is_empty() {
+                            file.name.clone()
+                        } else {
+                            "???".to_owned()
+                        };
+
+                        let mut additional_info = vec![];
+                        if !file.mime.is_empty() {
+                            additional_info.push(format!("type: {}", file.mime));
+                        }
+                        if let Some(bytes) = &file.bytes {
+                            additional_info.push(format!("{} bytes", bytes.len()));
+                        }
+                        if !additional_info.is_empty() {
+                            info += &format!(" ({})", additional_info.join(", "));
+                        }
+
+                        ui.label(info);
+                    }
+                });
+            }
+        });
+
+        preview_files_being_dropped(ctx);
+
+        // Collect dropped files:
+        ctx.input(|i| {
+            if !i.raw.dropped_files.is_empty() {
+                self.dropped_files.clone_from(&i.raw.dropped_files);
+            }
+        });
+    }
+}
+
+fn preview_files_being_dropped(ctx: &egui::Context) {
+    use egui::{Align2, Color32, Id, LayerId, Order, TextStyle};
+    use std::fmt::Write as _;
+
+    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
+        let text = ctx.input(|i| {
+            let mut text = "Dropping files:\n".to_owned();
+            for file in &i.raw.hovered_files {
+                if let Some(path) = &file.path {
+                    write!(text, "\n{}", path.display()).ok();
+                } else if !file.mime.is_empty() {
+                    write!(text, "\n{}", file.mime).ok();
+                } else {
+                    text += "\n???";
+                }
+            }
+            text
+        });
+
+        let painter =
+            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
+
+        let screen_rect = ctx.screen_rect();
+        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
+        painter.text(
+            screen_rect.center(),
+            Align2::CENTER_CENTER,
+            text,
+            TextStyle::Heading.resolve(&ctx.style()),
+            Color32::WHITE,
+        );
+    }
 }
