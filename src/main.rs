09eae1bdd7d7172a61b7d695171fe523ed220268
Szalló László <szallol@gmail.com>
2024-09-16 17:26:45 +0300

    convert button added

--- src/main.rs
+++ src/main.rs
@@ -72,11 +72,10 @@ struct ObjInfo {
 
 #[derive(Default)]
 struct MyApp {
-    dropped_files: Vec<egui::DroppedFile>,
     obj_source_path: Option<String>,
     obj_files: Option<Vec<ObjInfo>>,
-    metadata_path: Option<String>,
-    dest_path: Option<String>,
+    conver_enabled: bool,
+    // dest_path: Option<String>,
 }
 
 impl eframe::App for MyApp {
@@ -88,6 +87,7 @@ impl eframe::App for MyApp {
                 if let Some(path) = rfd::FileDialog::new().pick_folder() {
                     self.obj_source_path = Some(path.display().to_string());
                     self.obj_files = all_obj_files_recursively(&path.display().to_string());
+                    self.conver_enabled = true;
                 }
             }
 
@@ -106,79 +106,15 @@ impl eframe::App for MyApp {
                 });
             };
 
-            // Show dropped files (if any):
-            if !self.dropped_files.is_empty() {
-                ui.group(|ui| {
-                    ui.label("Dropped files:");
-
-                    for file in &self.dropped_files {
-                        let mut info = if let Some(path) = &file.path {
-                            path.display().to_string()
-                        } else if !file.name.is_empty() {
-                            file.name.clone()
-                        } else {
-                            "???".to_owned()
-                        };
-
-                        let mut additional_info = vec![];
-                        if !file.mime.is_empty() {
-                            additional_info.push(format!("type: {}", file.mime));
-                        }
-                        if let Some(bytes) = &file.bytes {
-                            additional_info.push(format!("{} bytes", bytes.len()));
-                        }
-                        if !additional_info.is_empty() {
-                            info += &format!(" ({})", additional_info.join(", "));
-                        }
-
-                        ui.label(info);
+            if self.obj_files.is_some() {
+                ui.add_enabled_ui(self.conver_enabled, |ui| {
+                    if ui.button("Convert..").clicked() {
+                        debug!("convert clicked");
+                        self.conver_enabled = false;
                     }
                 });
             }
         });
-
-        preview_files_being_dropped(ctx);
-
-        // Collect dropped files:
-        ctx.input(|i| {
-            if !i.raw.dropped_files.is_empty() {
-                self.dropped_files.clone_from(&i.raw.dropped_files);
-            }
-        });
-    }
-}
-
-fn preview_files_being_dropped(ctx: &egui::Context) {
-    use egui::{Align2, Color32, Id, LayerId, Order, TextStyle};
-    use std::fmt::Write as _;
-
-    if !ctx.input(|i| i.raw.hovered_files.is_empty()) {
-        let text = ctx.input(|i| {
-            let mut text = "Dropping files:\n".to_owned();
-            for file in &i.raw.hovered_files {
-                if let Some(path) = &file.path {
-                    write!(text, "\n{}", path.display()).ok();
-                } else if !file.mime.is_empty() {
-                    write!(text, "\n{}", file.mime).ok();
-                } else {
-                    text += "\n???";
-                }
-            }
-            text
-        });
-
-        let painter =
-            ctx.layer_painter(LayerId::new(Order::Foreground, Id::new("file_drop_target")));
-
-        let screen_rect = ctx.screen_rect();
-        painter.rect_filled(screen_rect, 0.0, Color32::from_black_alpha(192));
-        painter.text(
-            screen_rect.center(),
-            Align2::CENTER_CENTER,
-            text,
-            TextStyle::Heading.resolve(&ctx.style()),
-            Color32::WHITE,
-        );
     }
 }
 
